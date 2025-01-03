d7392016a02a49bb455b806ebb052310559227aa
monkaCode <f.h612@hotmail.com>
2025-01-03 16:36:14 +0100

    added mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import './page.css';
+import { saveInterest } from '@/actions/saveInterest';
 
 export default function Home() {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+
+  const handleSubmit = async () => {
+    await saveInterest(name, email);
+  }
+
   return (<div style={{
     display: 'flex',
     flexDirection: 'column',
@@ -11,7 +20,8 @@ export default function Home() {
   }}>
     <h1 style={{fontSize: '4rem'}}>Wir haben Dein Interesse geweckt?</h1>
     <span style={{fontSize: '2rem'}}>Dann melde dich bei uns!</span>
-    <input style={{fontSize: '3rem', width: '40%', color: 'black'}} type="text" placeholder="E-Mail Addresse"/>
-    <button className="button" style={{height: '3rem'}}>Ich bin interessiert!</button>
+    <input style={{fontSize: '3rem', width: '40%', color: 'black'}} type="text" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
+    <input style={{fontSize: '3rem', width: '40%', color: 'black'}} type="text" placeholder="E-Mail Addresse" onChange={(e) => setEmail(e.target.value)}/>
+    <button className="button" style={{height: '3rem'}} onClick={handleSubmit}>Ich bin interessiert!</button>
   </div>);
 }
---
+++ src/db.ts
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI as string).then(() => { console.log('Connected to MongoDB'); });
